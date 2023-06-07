33c7b519feb6dc88e76c5c46bddd5c1e9f2f4821
Manicrouns <manikandan.karunakaran@ideaxecution.com>
2023-06-07 12:57:56 +0530

    first commit

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,12 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="la la-user-circle nav-icon" aria-hidden="true"></i> Users</a></li>
+
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('tag') }}"><i class="la la-television nav-icon" aria-hidden="true"></i> Movie Menu</a></li>
+
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('setting') }}"><i class="la la-cogs nav-icon" aria-hidden="true"></i> {{ trans('Settings') }}</a></li>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Movie-x!</title>
+    <style>
+       
+       
+        body {
+            margin :0;
+            padding :0;
+            width :100%;
+            height :100vh;
+            background-size : cover;
+            background-image: url("https://cdn5.vectorstock.com/i/1000x1000/73/99/movie-citema-seat-hall-interior-background-vector-20707399.jpg");
+            font-family: Arial, sans-serif;
+        }
+        
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+        
+        h1 {
+            color: #333;
+            font-size: 32px;
+            margin-bottom: 20px;
+        }
+        
+        p {
+            color: #666;
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+        
+        .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #ff4f00;
+            color: #fff;
+            font-size: 20px;
+            text-decoration: none;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+        
+        .btn:hover {
+            background-color: #ff6000;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome to Movie-x</h1>
+        <p>Discover the world of movies and indulge in cinematic bliss.</p>
+        <p>Whether you're a film enthusiast or simply looking for some entertainment, Movie-x has got you covered.</p>
+        <a href="/admin/login" class="btn">Explore Movies</a>
+    </div>
+</body>
+</html>
